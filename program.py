46a62f8775294ca45ad0ce7d0ea3a40039136289
VilhelmsLittleHelper <45451577+VilhelmsLittleHelper@users.noreply.github.com>
2018-12-01 13:47:41 +0100

    First of december
    Vilhelm's little helper comes out to help!

---
+++ program.py
@@ -0,0 +1,44 @@
+def day1():
+    day1part1()
+    day1part2()
+
+day1()
+
+
+def day1part1():
+    freq = 0
+
+    file = open("values.txt", "r")
+    values = file.readlines()
+
+    vector = []
+    for value in values:
+        vector.append(freq)
+        number = int(value.replace('\n', ''))
+        freq = freq + number
+
+    print(freq)
+
+
+def day1part2():
+    freq = 0
+
+    file = open("values.txt", "r")
+    values = file.readlines()
+
+    vector = []
+    searching = True
+
+    while searching:
+        for value in values:
+            if freq in vector:
+                searching = False
+                print(freq)
+                break
+
+            if not searching:
+                break
+
+            vector.append(freq)
+            number = int(value.replace('\n', ''))
+            freq = freq + number
